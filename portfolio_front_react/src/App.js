3dd908d6be727ce40fe52732039941122e7a02bc
최진수 <cjs@choejinsuui-MacBookPro.local>
2024-01-10 22:23:36 +0900

    프로젝트 생성

---
+++ portfolio_front_react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Project Start!
+      Test
+      Test
+    </div>
+  );
+}
+
+export default App;
