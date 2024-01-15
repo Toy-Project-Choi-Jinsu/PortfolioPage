54795eae3e6129d4c7c5118a93818b440c34d318
최진수 <cjs@choejinsuui-MacBookPro.local>
2024-01-15 16:53:14 +0900

    [Front-추가]맨위로 버튼 구현

--- portfolio_front_react/src/App.js
+++ portfolio_front_react/src/App.js
@@ -6,20 +6,27 @@ import Awards from "./components/3.Awards/Awards";
 import Project from "./components/4.Project/Project";
 
 import styled from "styled-components"
+import { GoMoveToTop } from "react-icons/go";
 
 function App() {
+  const pageRef = useRef();
   const aboutMeRef = useRef();
   const trainingRef = useRef();
   const awardsRef = useRef();
   const projectRef = useRef();
 
+  const moveToTop = () => {
+    pageRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
-    <PortfolioPage>
+    <PortfolioPage ref={pageRef}>
       <Contentes>
         <AboutMe aboutMe={aboutMeRef} />
         <Training training={trainingRef} />
         <Awards awards={awardsRef} />
         <Project project={projectRef} />
+        <GoMoveToTop onClick={moveToTop} />
       </Contentes>
       <SideBar
         aboutMe={aboutMeRef}
@@ -44,4 +51,16 @@ const Contentes = styled.div`
 width: 70%;
 padding: 30px 10% 30px 10%;
 /* border: 1px solid black; */
+  & svg{
+    position: fixed;
+    right: 18%;
+    bottom: 2%;
+    width: 30px;
+    height: 30px;
+    padding: 5px;
+    border-radius: 50%;
+    color: white;
+    background-color: #5477AD;
+    cursor: pointer;
+  }
 `
